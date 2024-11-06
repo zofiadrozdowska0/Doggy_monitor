e84f7255acd8a1ab71a6d583bf0907e37a853a80
mrog357 <s184435@student.pg.edu.pl>
2024-11-06 16:16:57 +0100

    Aplikacja Mobilna

---
+++ app/DogMonitor/app/src/main/java/com/example/dogmonitor/ui/statistics/StatisticsFragment.kt
@@ -0,0 +1,42 @@
+package com.example.dogmonitor.ui.statistics
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.dogmonitor.databinding.FragmentStatisticsBinding
+
+class StatisticsFragment : Fragment() {
+
+    private var _binding: FragmentStatisticsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val statisticsViewModel =
+            ViewModelProvider(this).get(StatisticsViewModel::class.java)
+
+        _binding = FragmentStatisticsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textNotifications
+        statisticsViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
